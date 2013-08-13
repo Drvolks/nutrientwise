3b96059a42703b1d6ebf157cb43f6d78ea549c92
drvolks <5374619+drvolks@users.noreply.github.com>
2013-08-13 19:02:08 -0400

    #73

---
+++ Nutrient Wise/NutrientValueCell.h
@@ -0,0 +1,13 @@
+//
+//  NutrientValueCell.h
+//  Nutrient Wise
+//
+//  Created by drvolks on 2013-08-13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NutrientValueCell : UITableViewCell
+
+@end

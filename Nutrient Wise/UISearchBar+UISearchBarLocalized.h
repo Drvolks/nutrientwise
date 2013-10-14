866a63b7641dba91ecc59d71af024b7240f23917
drvolks <5374619+drvolks@users.noreply.github.com>
2013-10-14 16:33:55 -0400

    UISearchBar avec texte custom pour bouton annuler

---
+++ Nutrient Wise/UISearchBar+UISearchBarLocalized.h
@@ -0,0 +1,15 @@
+//
+//  UISearchBar+UISearchBarLocalized.h
+//  Nutrient Wise
+//
+//  Created by drvolks on 2013-10-14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISearchBar (UISearchBarLocalized)
+
+- (void)cancelButton:(NSString*)text;
+
+@end

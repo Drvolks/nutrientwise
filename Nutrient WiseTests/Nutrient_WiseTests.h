e04fd0ec1a92b7bcbef5f7d2254ad3581ab4427e
drvolks <5374619+drvolks@users.noreply.github.com>
2014-09-07 10:52:24 -0400

    Retirer les warning xcode 6

--- Nutrient WiseTests/Nutrient_WiseTests.h
+++ Nutrient WiseTests/Nutrient_WiseTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface Nutrient_WiseTests : SenTestCase
+@interface Nutrient_WiseTests : XCTestCase
 
 @end

4925ac6b2604a9e249443047200a6dbac97a1083
Jean-Francois Dufour <jfdufour@me.com>
2017-09-13 18:12:55 -0400

    pods

---
+++ Pods/Target Support Files/Pods-Nutrient WiseTests/Pods-Nutrient WiseTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Nutrient_WiseTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Nutrient_WiseTestsVersionString[];
+

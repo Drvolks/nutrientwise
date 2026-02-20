494ab8ea7da57b4f7061bc462f675f7079b969c4
drvolks <5374619+drvolks@users.noreply.github.com>
2026-02-19 19:07:10 -0500

    UIScene refactor

---
+++ Nutrient Wise/SceneDelegate.m
@@ -0,0 +1,39 @@
+#import "SceneDelegate.h"
+#import "AppDelegate.h"
+#import "MKiCloudSync.h"
+
+@implementation SceneDelegate
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
+    UIWindowScene *windowScene = (UIWindowScene *)scene;
+    AppDelegate *appDelegate = (AppDelegate *)UIApplication.sharedApplication.delegate;
+
+    [appDelegate setup];
+
+    self.window = [[UIWindow alloc] initWithWindowScene:windowScene];
+    [[NSBundle mainBundle] loadNibNamed:@"TabBarController" owner:appDelegate options:nil];
+    [self.window setRootViewController:appDelegate.rootController];
+    self.window.backgroundColor = [UIColor systemBackgroundColor];
+    [self.window makeKeyAndVisible];
+
+    [appDelegate setTabLabels];
+    [appDelegate pushManagedContextToViewControllers];
+    [MKiCloudSync start];
+}
+
+- (void)sceneDidDisconnect:(UIScene *)scene {
+}
+
+- (void)sceneDidBecomeActive:(UIScene *)scene {
+}
+
+- (void)sceneWillResignActive:(UIScene *)scene {
+}
+
+- (void)sceneWillEnterForeground:(UIScene *)scene {
+}
+
+- (void)sceneDidEnterBackground:(UIScene *)scene {
+}
+
+@end
